8a4cc18f34de2c5c06d52fad9e22511f24774922
Leonardo Carey Machado <leonardo.machado@PC-000518.local>
2024-09-24 13:54:11 -0300

    feat: el tipador finished

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,47 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let intervalTipador: number
+let textarea: HTMLElement | null
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+window.onload = function() {
+  textarea = document.getElementById('tipador-area')
+  const buttonStartTipador = document.querySelector('#start-tipador') 
+  const buttonStopTipador = document.querySelector('#stop-tipador')
+  
+  if (buttonStartTipador) {
+    buttonStartTipador.addEventListener('click', startElTipador)
+  }
+
+  if (buttonStopTipador) {
+    buttonStopTipador.addEventListener('click', stopTipador)
+  }
+}
+
+function startElTipador() {
+  console.log('[el-tipador-ativador:start]')
+
+  let times = 1
+
+  intervalTipador = setInterval(function() {
+    console.log(times)
+    if (textarea) {
+      textarea.focus()
+      const elephants = times === 1 ? `${times} elefante incomoda muita gente\n` : `${times} elefantes incomodam muito mais\n` 
+      textarea.innerHTML += elephants
+    }
+
+    times++
+  }, 1000)
+}
+
+function stopTipador() {
+  console.log('[el-tipador-ativador:stop]')
+
+  if (textarea) {
+    textarea.innerHTML = ''
+  }
+
+  if (intervalTipador) {
+    clearInterval(intervalTipador)
+  }
+}
